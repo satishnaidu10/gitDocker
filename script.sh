fbdb25dda998d93a40d47cd9ddd65cb3bf9fddc8
satishnaidu10 <48701677+satishnaidu10@users.noreply.github.com>
2019-10-29 11:38:27 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@ sudo docker stop $(sudo docker ps -a -q)
 sudo docker rm $(sudo docker ps -a -q)
 cd /home/ubuntu
 rm -rf dockerImageNode
-git clone https://github.com/andalike/dockerImageNode.git
+git clone https://github.com/satishnaidu10/dockerImageNode.git
 cd dockerImageNode
-sudo docker build -t andalike/node-web-app .
-sudo docker run -p 49160:8080 -d andalike/node-web-app
+sudo docker build -t satishnaidu10/node-web-app .
+sudo docker run -p 49160:8080 -d satishnaidu10/node-web-app
